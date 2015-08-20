3ddc7082f671965d441181d8c49b3f93b5123217
yminachi <yminachi@pillartechnology.com>
2015-08-20 15:22:56 -0400

    working hibernate spike

---
+++ api/src/main/java/User.java
@@ -0,0 +1,22 @@
+
+
+public class User{
+	public int id;
+	public String username;
+	
+	public int getId(){
+		return id;
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+}
