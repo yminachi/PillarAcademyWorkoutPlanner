8e0b43a5ec26da68168fc61e8290fc8bfda7c6d5
yminachi <yminachi@pillartechnology.com>
2015-08-20 11:15:23 -0400

    adding hibernate (not quite working)

---
+++ api/src/main/java/com/pillartechnology/academy/User.java
@@ -0,0 +1,22 @@
+package com.pillartechnology.academy;
+
+public class User{
+	public int id;
+	public String username;
+	
+	public int getId(){
+		return id;
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+}
